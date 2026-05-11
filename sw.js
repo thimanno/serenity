61c2b9c94872809686375f57c442bab632c7203f
thimanno <thimanno@gmail.com>
2026-05-10 23:16:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,17 @@
-const CACHE = 'serenity-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'serenity-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    Promise.all(keys.map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   if (e.request.url.includes('/api/')) return;
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
